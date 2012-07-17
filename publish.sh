8aae91b9883213684a687613cdf6d41b96a360de
Guillaume Matheron <guillaume.matheron.06@gmail.com>
2012-07-17 11:48:01 +0200

    Updated publish script

--- publish.sh
+++ publish.sh
@@ -8,11 +8,12 @@ for file in `ls *.md`
   rm `basename $file .md`_.md
 done
 
-echo Copying files onto server
-scp *.png *.html carpasinivores@198.245.54.228:/var/www/carpasinivores/doc
+echo Copying documentation and sources onto server
+scp *.png *.html *.js carpasinivores@198.245.54.228:/var/www/carpasinivores/doc
 
-for file in `ls *.html`
+for file2 in `ls *.md`
   do
+  file=`basename $file2 .md`.html
   echo Removing temp file $file
   rm $file
 done
