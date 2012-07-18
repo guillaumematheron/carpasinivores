607dfbac19a5642ea46a14874dce9b06403d9e2b
Guillaume Matheron <guillaume.matheron.06@gmail.com>
2012-07-18 16:25:35 +0200

    Still working on mobile devices integration

--- src/publish.sh
+++ src/publish.sh
@@ -3,16 +3,18 @@ echo "[![Logo](logo.png)](index.html)" >header
 for file in `ls *.md`
   do
   echo Converting file $file
-  cp header `basename $file .md`_.md
+  # cp header `basename $file .md`_.md
   cat $file >> `basename $file .md`_.md
   ./fr2html.sh `basename $file .md`_.md
   ./convertLinks.sh `basename $file .md`_.md
-  markdown `basename $file .md`_.md>../`basename $file .md`.html
+  markdown `basename $file .md`_.md>../`basename $file .md`_.html
+  cat headerHTML ../`basename $file .md`_.html > ../`basename $file .md`.html
+  rm ../`basename $file .md`_.html
   rm `basename $file .md`_.md
 done
 
 echo Copying documentation and sources onto server
-cp *.png simulator.html *.js ..
+cp *.png simulator.html *.js *.xml ..
 scp ../*.png ../*.html ../*.js carpasinivores@198.245.54.228:/var/www/carpasinivores
 
 rm header
